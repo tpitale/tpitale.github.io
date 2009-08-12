5c74825be294e7ab32431fd88d61f1cd3b032d4b
tpitale <tpitale@gmail.com>
2009-08-12 17:02:22 -0400

    new post on mongolytics updated styling, added gistify plugin

--- content/javascripts/main.js
+++ content/javascripts/main.js
@@ -1,4 +1,16 @@
+;(function($) {
+  $.fn.gistify = function() {
+    return this.each(function() {
+      this.innerHTML = 'Loading Gist '+this.id;
+      $.getJSON("http://gist.github.com/"+ this.id +".json?callback=?", function (gist) {
+        $('#'+gist.repo).replaceWith(gist.div);
+      });
+    });
+  }
+})(jQuery);
+
 $(document).ready(function(){
+  fill_in_gists();
   display_flickr($("div.flickr_badge_image", "#photos"), 0);
 });
 
@@ -8,4 +20,9 @@ function display_flickr(divs, at) {
   $($(divs).get(at)).fadeIn(250, function() {
     display_flickr(divs, at++);
   });
+}
+
+function fill_in_gists() {
+  $('div.gist').gistify();
+  $("head").append('<link rel="stylesheet" href="http://gist.github.com/stylesheets/gist/embed.css"/>');
 }
