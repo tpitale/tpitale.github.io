6f995989fa1febce41ec7cf0d47a9ca391a48cf0
Tony Pitale <tpitale@gmail.com>
2011-09-30 00:54:18 -0400

    styling and content fixes

--- javascripts/application.js
+++ javascripts/application.js
@@ -1,13 +1,13 @@
 ;(function($) {
   $.fn.gistify = function() {
     return this.each(function() {
-      var link = $(this);
+      var $link = $(this);
       var href = $(this).attr('href');
 
       var gist_id = /http:\/\/gist\.github\.com\/(\d+)/.exec(href);
 
       if (gist_id) {
-        link.wrap('<div id="embedded-gist-'+gist_id[1]+'">');
+        $link.before('<div id="embedded-gist-'+gist_id[1]+'">');
         jQuery.getJSON(href+'.json?callback=?', function(data) {
           var stylesheet_needed = true;
           $('head link[rel=stylesheet]').each(function() {
